48e1c01174d87587c3c719f94f55cc19f77c7789
Clara Castillo <claracastillochale@gmail.com>
2026-06-21 13:18:00 +0200

    refactor(_internal): move window-index-map out of drag-session (#1009) (#1014)

--- projects/forty-cdk/src/lib/table/table-row-reorder.ts
+++ projects/forty-cdk/src/lib/table/table-row-reorder.ts
@@ -13,7 +13,7 @@ import { type ForDragDropEvent } from '../drag-drop/drag-drop-context';
 import { ForDropList } from '../drag-drop/drop-list';
 import { FOR_DRAG_DROP_DEFAULTS } from '../drag-drop/drag-drop-defaults';
 import { LiveAnnouncer } from '../_internal/live-announcer/live-announcer';
-import { translateWindowReorder } from '../_internal/drag-session/window-index-map';
+import { translateWindowReorder } from '../_internal/window-index-map/window-index-map';
 import { injectTableContext } from './table-context';
 
 /** Payload of `rowReorder`: the previous and new row index. */
