af8f4771c9dd81c2b854f91de4c63f88b053db56
Clara Castillo <claracastillochale@gmail.com>
2026-07-06 11:12:42 +0200

    refactor(core): export FOCUSABLE_SELECTOR from the core barrel (#1299)

--- projects/forty-cdk/core/src/public-api.ts
+++ projects/forty-cdk/core/src/public-api.ts
@@ -197,7 +197,7 @@ export {
   type FloatingSide,
   injectFloating,
 } from './floating/floating';
-export { findFirstFocusable } from './focus-trap/focus-trap';
+export { FOCUSABLE_SELECTOR, findFirstFocusable } from './focus-trap/focus-trap';
 export { injectHasFocusableContent } from './focusable-content/focusable-content';
 export { FormUiControlBase } from './form-ui-control/form-ui-control-base';
 export { mirrorUnfocusedValue } from './form-ui-control/unfocused-value-mirror';
